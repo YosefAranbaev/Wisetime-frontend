54816bea15db0b27bb2a0558912abef431b8e1f6
yosef <aranbaev00@gmail.com>
2021-12-21 14:27:19 +0200

    form with validation

--- scripts/calendar.js
+++ scripts/calendar.js
@@ -38,3 +38,48 @@
 // }
 
 // getAllEvents();
+const getColor = () => {
+    if ($(".radio-input")[0].checked == true)
+        return "red";
+    if ($(".radio-input")[1].checked == true)
+        return "green";
+    if ($(".radio-input")[2].checked == true)
+        return "yellow";
+    if ($(".radio-input")[3].checked == true)
+        return "blue";
+}
+const fileErrorTreatment = (err) => {
+    console.log(err);
+    $(".fileError").html("");
+    if (err == 401) {
+        $(".fileError").append("Please fill in all the fields on the form!");
+    }
+    if (err == 403) {
+        $(".fileError").append("There is friction in the schedule. Please select another time!");
+    }
+    if (err == 400) {
+        $(".fileError").append("Error getting the data from db");
+    }
+}
+function check() {
+    const obj = "http://localhost:3000/api/tasks";
+    const newTask = {
+        "name": document.forms["myForm"]["name"].value,
+        "day": document.forms["myForm"]["day"].value,
+        "hour_start_time": document.forms["myForm"]["hour_start_time"].value,
+        "hour_end_time": document.forms["myForm"]["hour_end_time"].value,
+        "color": getColor()
+    };
+    const res = $.ajax({
+        type: "POST",
+        url: obj,
+        data: newTask,
+        success:(res)=>{
+            window.location.href="http://127.0.0.1:5500/wisetime-frontend/home.html";
+        },
+        error: (response) => {
+            fileErrorTreatment(response.status);
+        }
+    });
+    return false;
+}
--- scripts/schedule.js
+++ scripts/schedule.js
@@ -66,8 +66,8 @@ const hours = {
    "23:00":"tth",
 }
 
-const do_ = () => {
-   get_time_set().then(res => {
+const addTaskToSchedule = () => {
+   getTimelist().then(res => {
        const j = res.json().then(result => {
            console.log("dd"+result);
            console.log("08:01" < "09:00");
@@ -96,7 +96,7 @@ const do_ = () => {
 //    return new Promise(resolve => setTimeout(resolve, ms));
 // }
 
-const get_time_set = async () => {
+const getTimelist = async () => {
    try {
        const response = fetch('http://localhost:3000/api/tasks', { method: 'GET' })
        if (response) {
