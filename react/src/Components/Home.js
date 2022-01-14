695745feb8b7823dc4af4f09d4a6af31ea0dcf8c
karina <karina4nosenko@gmail.com>
2022-01-14 21:15:43 +0200

    Handling different responses from server on login

--- react/src/Components/Home.js
+++ react/src/Components/Home.js
@@ -4,7 +4,7 @@ const Home = () => {
 
     return ( 
         <div className='home'>
-            Success.
+            Homepage
         </div>
     );
 }
--- react/src/Components/Login.js
+++ react/src/Components/Login.js
@@ -5,15 +5,25 @@ const serviceUrl = 'http://localhost:8080';
 const Login = () => {
 
     const handleLogin = () => {
-        fetch(`${serviceUrl}/api/auth/login`, {
-            method: 'POST'
+        fetch(`${serviceUrl}/api/auth/signin`, {
+            method: 'POST',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                'email': 'karina@gmail.com',
+                'password': '111'
+            })
         })
         .then((response) => {
             if(response.ok) {
                 alert('ok');
-            } else {
-                alert('not ok');
-            }   
+            } else if(response.status === 401) {
+                alert('Invalid password');
+            } else if(response.status === 500) {
+                alert('Invalid email');
+            }    
         })
     };
 
